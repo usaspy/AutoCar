566ca548d0133580429c0ac357264bf9eb4f9870
zhanghong <11989935@qq.com>
2018-04-27 16:47:42 +0800

    initial

--- autocar/ultrasonic.py
+++ autocar/ultrasonic.py
@@ -1,56 +1,69 @@
 #!/usr/bin/python3
 # coding=utf-8
-# 超声波测距
+# 超声波测距系统
+#  0.3 米 =近进告警  0.1米=抵近停止
+# 具备下列功能：
+#1.实时采集当前正前方（7.5）的障碍物距离，如果小于0.1米立即发送停止前进的命令
+#2.同时从左至右采集四个方向的距离4.5、6、9、10.5的距离（探测完成后回到7.5正前方）
+#3.如果存在无障碍的方向( >0.3米)，即随机从几个方向点上选择一个执行转弯程序（转弯完成后，超声波检测恢复），之后继续行走并执行步骤1
+#4.如果都有障碍，表示无法绕过，则控制车辆倒退1秒，继续执行步骤2
 import RPi.GPIO as GPIO
-import atexit
 import time
 
-atexit.register(GPIO.cleanup)
+STATUS = "OK" #一切正常
+
 GPIO.setmode(GPIO.BOARD)
 #SG90马达
-used_pin1 = 8
+sg90_pin = 8
+
+direction1=4.5
+direction2=6
+direction3=9
+direction4=10.5
+direction0=7.5 #正前方
+
 #超声波
-echo = 13
-trig=11
+echo_pin = 13
+trig_pin=11
 
-#测距系统初始化
-def init():
-    #转向马达初始化:
-    GPIO.setup(used_pin1, GPIO.OUT, initial=False)
-    p = GPIO.PWM(used_pin1, 50)
-    p.start(7.5)  #超声波初始角度
+alert_dist=0.3 #0.3米
+stop_dist=0.1 #0.1米
 
-    #超声波初始化
-    GPIO.setup(trig, GPIO.OUT, initial=GPIO.LOW)
-    GPIO.setup(echo, GPIO.IN)
+#转向马达初始化:
+GPIO.setup(sg90_pin, GPIO.OUT, initial=False)
+p = GPIO.PWM(sg90_pin, 50)
+p.start(direction0)  #超声波对正前方
 
-#转向一个角度开始测距 PWM在2.5~12.5
-def __distance(p,PWM):
-    print("starting...")
+#超声波初始化
+GPIO.setup(trig_pin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(echo_pin, GPIO.IN)
+
+#系统复位
+def rest():
+    GPIO.cleanup()
+
+#转向到一个指定角度并开始测距，返回一个距离值
+def __distance(direction):
     #先转向指定角度
-    p.ChangeDutyCycle(PWM)
+    p.ChangeDutyCycle(direction)
     time.sleep(0.02)
 
     #开始测距
-    GPIO.output(trig, GPIO.HIGH)
+    GPIO.output(trig_pin, GPIO.HIGH)
     time.sleep(0.0001)
 
-    GPIO.output(trig, GPIO.LOW)
+    GPIO.output(trig_pin, GPIO.LOW)
     #改成事件监听更省资源
-    while GPIO.input(echo) == GPIO.LOW:
+    while GPIO.input(echo_pin) == GPIO.LOW:
         pass
     starttime = time.time()
-    while GPIO.input(echo) == GPIO.HIGH:
+    while GPIO.input(echo_pin) == GPIO.HIGH:
         pass
     endtime = time.time()
     distance = 340 * (endtime - starttime) / 2
-    print("distance:", distance)
+    print("direction:",direction,"   distance:", distance)
 
-    #如果距离小于1分米报警
-    if distance < 0.1:
-        return "Alarm"
-    else:
-        return "OK"
+    return distance
 
 #超声波子系统准备就绪，等待指令
 def standby(dd):
