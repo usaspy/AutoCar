3ecf9d46413196458dbccac902ad6947f7ae6a13
zhanghong <zhanghongyf@dhcc.com.cn>
2018-05-03 20:19:28 +0800

    ok

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from multiprocessing import Process
 from autocar import wheel
 from autocar import controller
 from autocar import ultrasonic
+from autocar import camera
 from autocar import dd   #控制通道  进程间数据共享
 
 
@@ -12,15 +13,19 @@ if __name__=="__main__":
     p1 = Process(target=wheel.standby,args=(dd,),name='wheel')  #行走系统
     p2 = Process(target=controller.webconsole,args=(dd,),name='controller') #用户操纵系统
     p3 = Process(target=ultrasonic.standby,args=(dd,),name='ultrasonic') #超声波测距系统
+    p4 = Process(target=camera.Camera,args=(dd,),name='Camera') #照相子系统
 
     p1.daemon = True
     p2.daemon = True
     p3.daemon = True
+    p4.daemon = True
     p1.start()
     p2.start()
     p3.start()
+    p4.start()
     p1.join()
     p2.join()
     p3.join()
+    p4.join()
 
     print("系统停机...")
